87257596473cf0ad33a759a65c038362299e2866
Tehuti English <ausareenglish@gmail.com>
2020-03-27 23:56:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  // declaring and assigning empty array
+  var beatlesPlay = [];
+  // 'for loop' that iterates over array of musicians
+  for (let i = 0; i < musicians.length; i++ ) {
+      //create a new string from musician and instrument arrays
+      
+      beatlesPlay.push(`${musicians[i]} plays ${instruments[i]}` )
+  }
+  // return the array of new array of strings containing what instruments each
+  // musician plays
+  
+  return beatlesPlay;
+}
