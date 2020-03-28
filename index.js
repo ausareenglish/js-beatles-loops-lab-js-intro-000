a9da3df6759c3ed6e1683984401eb2f73c814258
Tehuti English <ausareenglish@gmail.com>
2020-03-28 00:02:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function theBeatlesPlay(musicians, instruments) {
   return beatlesPlay;
 }
 
+function johnLennonFacts (lennonFacts) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  function lennonCounter () {
+      let i = 0;
+      i++;
+}
+  while (i < facts.length) {
+  
+  facts.push(...lennonFacts[i], [])
+  
+  
+  
+  
+}
