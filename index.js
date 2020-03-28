a4775122c499d585f48e88927ff9f2c078729079
Tehuti English <ausareenglish@gmail.com>
2020-03-28 00:28:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(lennonFacts) {
+  
+  // creates varaible wih empty array 
+  
+  let facts2 = [];
+  
+  //creates array with John Lennon facts
+  
   const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
@@ -22,14 +29,18 @@ function johnLennonFacts(lennonFacts) {
   "He hated the sound of his own voice"
 ];
 
+// creates counter function
+
 let facts2 = [];
   function lennonCounter() {
       let i = 0;
       i++;
 }
+
+// for loop that adds "!!!" to ever index in 
   while (lennonCounter() < facts.length) {
   
-   let facts2 facts.push(lennonFacts[i] + "!!!")
+   let facts2 = facts2.push(lennonFacts[i] + "!!!")
   
   
   
